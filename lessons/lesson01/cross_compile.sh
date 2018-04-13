88e062ac71b810138f40f6996a2fc04058217db3
wusuopubupt <wdxbupt2009@gmail.com>
2018-04-13 23:26:13 +0800

    add cross platform compile script

---
+++ lessons/lesson01/cross_compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#echo "build http-linux"
+#GOOS=linux GOARCH=amd64 go build -o bin/http-linux http.go
+
+echo "build http-osx"
+GOOS=darwin GOARCH=amd64 go build  -o bin/http-osx http.go
+
+#echo "build http-windows"
+#GOOS=windows GOARCH=386 go build -o bin/http-win http.go
