a4a14601f2e5e732239f3b1d9be1c0534a47dd88
wusuopubupt <wdxbupt2009@gmail.com>
2018-04-13 22:19:51 +0800

    update comment

--- lessons/lesson01/hello.go
+++ lessons/lesson01/hello.go
@@ -1,7 +1,12 @@
+// package
 package main
 
+// std lib
 import "fmt"
 
-func main() {
+// the entrance
+func main() { // go fmt
 	fmt.Println("hello golang")
+	// support Unicode
+	fmt.Println("hello go语言")
 }
