e3295cd28b0f9abb4b92d0a21328bed7d700a902
dongxu.wdx <dongxu.wdx@antfin.com>
2018-05-02 23:19:20 +0800

    import fmt package

--- lessons/lesson03/str_op.go
+++ lessons/lesson03/str_op.go
@@ -1,13 +1,20 @@
 package main
 
+import (
+    "fmt"
+)
+
 func main() {
 	// 相加
 	s1 := "hello " + "world"
+    fmt.Println(s1)
 
 	// 取字符
 	var c1 byte
 	c1 = s1[0]
+    fmt.Println(string(c1))
 
 	// 切片
 	s2 := s1[0:3]
+    fmt.Println(s2)
 }
