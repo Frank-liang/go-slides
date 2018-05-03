522b08147bbb4a6637fc16bd07280c9943508d52
dongxu.wdx <dongxu.wdx@antfin.com>
2018-05-03 14:32:47 +0800

    add simple expression calculator

---
+++ lessons/lesson03/expr.go
@@ -0,0 +1,36 @@
+package main
+
+import  (
+    "fmt"
+    "os"
+    "strconv"
+)
+
+func main() {
+
+    for k,v := range os.Args {
+        fmt.Println(k, v)
+    }
+
+    if len(os.Args) < 4 {
+        fmt.Println("Usage: go run expr.go 1 + 2")    
+    }
+
+    x, _ := strconv.Atoi(os.Args[1])
+    op := os.Args[2]
+    y, _ := strconv.Atoi(os.Args[3])
+
+    fmt.Println("operation is: ", op)
+
+    switch op {
+        case "+":
+            fmt.Printf("x %s y = %d\n", op, x + y)
+        case "-":
+            fmt.Printf("x %s y = %d\n", op, x - y)
+        case "*":
+            fmt.Printf("x %s y = %d\n", op, x * y)
+        case "/":
+            fmt.Printf("x %s y = %d\n", op, x / y)
+    }
+}
+
