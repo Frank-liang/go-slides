34a75e03c0a79efb4b4a1bb83b2cbd2fa99d8609
dongxu.wdx <dongxu.wdx@antfin.com>
2018-05-03 10:38:46 +0800

    update str operation example

--- lessons/lesson03/str_op.go
+++ lessons/lesson03/str_op.go
@@ -12,9 +12,11 @@ func main() {
 	// 取字符
 	var c1 byte
 	c1 = s1[0]
+    fmt.Println(c1)
     fmt.Println(string(c1))
+    fmt.Printf("%d %c\n", c1, c1)
 
 	// 切片
-	s2 := s1[0:3]
+	s2 := s1[0:len(s1)]
     fmt.Println(s2)
 }
