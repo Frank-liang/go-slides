5d9c98d667132f4c4c558bdc3bbf3cceb5bb3d4d
wusuopubupt <wdxbupt2009@gmail.com>
2018-05-11 13:44:38 +0000

    update slide

--- lessons/lesson04/json_encode.go
+++ lessons/lesson04/json_encode.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Student struct {
-	Id   int
-	Name string
+    Id   int `json:"-"`
+    Name string `json:"name,omitempty"`
 }
 
 func main() {
-	s := Student{
-		Id:   2,
-		Name: "alice",
-	}
+    //短变量声明 ？
+    var s Student
+    s.Id = 1
+    s.Name = "Alice"
 	buf, err := json.Marshal(s)
 	if err != nil {
 		log.Fatalf("marshal error:%s", err)
