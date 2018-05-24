f1fdb7d3f8b7a5c694558ae22f4a6ae3f835d62d
wusuopubupt <wdxbupt2009@gmail.com>
2018-05-24 23:46:39 +0800

    update slides

--- lessons/lesson06/func.go
+++ lessons/lesson06/func.go
@@ -9,12 +9,13 @@ type Point struct {
 	X, Y float64
 }
 
-func (p *Point) Distance(q Point) float64 {
+func Distance(p, q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
 func main() {
-	p := Point{1, 2}
-	q := Point{4, 6}
+	p := Point{0, 0}
+	q := Point{3, 4}
+	fmt.Println(Distance(p, q)) // function call
 	fmt.Println(p.Distance(q))
 }
